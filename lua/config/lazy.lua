af2662c22d12782d25ff61605bb44c36237fec0d
Lucas <lucasviana0604@gmail.com>
2025-07-15 22:52:58 -0300

    remove unused telescope keymaps

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,13 +1,13 @@
 -- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out,                            "WarningMsg" },
-      { "\nPress any key to exit..." },
+      { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
+      { out,                            'WarningMsg' },
+      { '\nPress any key to exit...' },
     }, true, {})
     vim.fn.getchar()
     os.exit(1)
@@ -24,12 +24,12 @@ autocmd('LspAttach', {
   group = group,
   callback = function(e)
     local opts = { buffer = e.buf }
-    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set('n', '<leader>vd', function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set('n', '<leader>vca', function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set('n', '<leader>vrn', function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set('i', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
   end
 })
 
@@ -37,24 +37,20 @@ vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
 
-require("lazy").setup({
+require('lazy').setup({
   spec = {
-    {
-      'Shatur/neovim-ayu',
-      config = function()
-        require('ayu').setup({
-          mirage = false,
-          terminal = false,
-          overrides = {}
-        })
-        vim.cmd.colorscheme "ayu"
-      end
-    },
-    { import = "plugins" },
+    --'scottmckendry/cyberdream.nvim',
+
+    'nyngwang/nvimgelion',
+    lazy = false,
+    priority = 1000,
+    { import = 'plugins' },
   },
-  install = { colorscheme = { "ayu" } },
+  install = { colorscheme = { 'gruvbox' } },
   change_detection = {
     enabled = false,
     notify = false,
   }
 })
+
+vim.cmd.colorscheme 'nvimgelion'
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -25,11 +25,5 @@ return {
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
     vim.keymap.set('n', '<leader>fl', builtin.live_grep, {})
-    vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-    vim.keymap.set('n', '<leader>fw', function()
-      local word = vim.fn.expand('<cword>')
-      builtin.grep_string({ search = word })
-    end)
-    vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
   end
 }
